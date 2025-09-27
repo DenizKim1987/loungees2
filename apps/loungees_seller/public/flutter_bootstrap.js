38c8e1955d381ec564d9656b68c3e0524feadd1f
DenizKim1987 <164604681+DenizKim1987@users.noreply.github.com>
2025-09-28 01:29:52 +0900

    .

--- apps/loungees_seller/public/flutter_bootstrap.js
+++ apps/loungees_seller/public/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "950867126"
+    serviceWorkerVersion: "2413337533"
   }
 });
